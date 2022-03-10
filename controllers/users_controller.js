f1ed33e12b8662ab7f9aaf296f6131bdd5c0fdc5
unknown <akshatparagg@gmail.comgit config --global user.name akshatparag>
2022-03-10 20:24:34 +0530

    Linked Sign Up page to Database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 //renders user profile page
 module.exports.profile = function(req,res){
     return res.render('user_profile.ejs',{
@@ -21,10 +23,40 @@ module.exports.signIn = function(req,res){
 
 //get signup data
 module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
 
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('Error in finding user');return;}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in finding user in signing up');return;}
+                console.log("Created");
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 //sign in and create a session for user
 module.exports.createSession = function(req,res){
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in signing up');return;}
 
-}
+        if(user){
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id',user_id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+};
