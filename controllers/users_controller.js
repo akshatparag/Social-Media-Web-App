b26dee990730c155bef52b84b1cef19b66a64a8d
Akshat Parag <akshatparagg@gmail.com>
2022-04-15 19:06:21 +0530

    Authentication using passport.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,13 +2,18 @@ const User = require('../models/user')
 
 //renders user profile page
 module.exports.profile = function(req,res){
-    return res.render('user_profile.ejs',{
-        title:"Profile"
+    return res.render('user_profile',{
+        title:"Profile",
+        user:req.user
     });
 };
 
 //renders sign up page
 module.exports.signUp = function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title:"Codeial | Sign Up"
     })
@@ -16,6 +21,9 @@ module.exports.signUp = function(req,res){
 
 //renders sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title:"Codeial | Sign in"
     })
@@ -44,5 +52,10 @@ module.exports.create = function(req,res){
 
 //sign in and create a session for user
 module.exports.createSession = function(req,res){
-    //
+    return res.redirect('/');
 };
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+    return res.redirect('/');
+}
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -18,10 +22,31 @@ app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
 //use express router
-app.use('/',require('./routes'));
-
 app.set('view engine','ejs');
-app.set('views','./views'); 
+app.set('views','./views');
+app.use(session({
+    name: 'codeial',
+    //TODO chnge the secret before deployement
+    secret: 'blahsomething',    //Any random
+    saveUninitialized:false,
+    resave:false,       //
+    cookie:{
+        maxAge:(1000 * 60 * 100)    //Cookie will expire after this much millisec
+    },
+    store: new MongoStore({
+            mongooseConnection:db,
+            autoRemove: 'disabled'
+    },
+    function(err){
+        console.log(err || 'connect-mongodb setup ok')
+    }
+    )
+})); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
