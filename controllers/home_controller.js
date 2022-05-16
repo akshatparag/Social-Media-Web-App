b70ad63d4f0687c9d2645ad896f91439813ff5fe
Akshat Parag <akshatparagg@gmail.com>
2022-05-16 20:14:38 +0530

    Added comment feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,37 @@
+const Post = require('../models/post')
+const Comment = require('../models/comment')
 module.exports.home = function(req,res){
     console.log(req.cookies);
-    return res.render('home.ejs',{
-        title:"Home"
-    });
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    //Populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+
+        return res.render('home.ejs',{ 
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
+   
 };
 
 //module.exports.actionName = function(req,res){}
--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
-const sassMiddleware = require('node-sass-middleware');
+const sassMiddleware =require('node-sass-middleware');
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
-    dest:'./assets/css',
+    dest: './assets/css',
     debug: true,
-    outputStyle:'extended',
-    prefix:'/css'
+    outputStyle: 'extended',
+    prefix: '/css'
 }));
-
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -25,43 +26,52 @@ app.use(cookieParser());
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-//extract style and scripts from subpages into layout
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
 
-//use express router
-app.set('view engine','ejs');
-app.set('views','./views');
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
-    //TODO chnge the secret before deployement
-    secret: 'blahsomething',    //Any random
-    saveUninitialized:false,
-    resave:false,       //
-    cookie:{
-        maxAge:(1000 * 60 * 100)    //Cookie will expire after this much millisec
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore({
-            mongooseConnection:db,
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
             autoRemove: 'disabled'
-    },
-    function(err){
-        console.log(err || 'connect-mongodb setup ok')
-    }
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
     )
-})); 
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/',require('./routes'));
+app.use(passport.setAuthenticatedUser);
 
-app.listen(port,function(err){
-    if(err){
-    // console.log("ERROR :" , err);
-    console.log(`Error in running : ${err}`);
-    return;
+// use express router
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Server is running on port : ${port}`);
+
+    console.log(`Server is running on port: ${port}`);
 });
